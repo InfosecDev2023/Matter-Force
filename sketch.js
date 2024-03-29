a8c13d9654f2bbc7b45385b2ec2574eac0793b78
InfosecDev2023 <145069871+InfosecDev2023@users.noreply.github.com>
2024-03-29 13:49:57 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -15,31 +15,7 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   
-   var ball_options = {
-    restitution: 0.95,
-    frictionAir:0.01
-  }
-   
-   var ground_options ={
-     isStatic: true
-   };
-  
-  btn2 = createImg('up.png');
-  btn2.position(350,30);
-  btn2.size(50,50);
-  btn2.mouseClicked(vForce);
 
-  ground1 = Bodies.rectangle(100,400,400,20,ground_options);
-  World.add(world,ground1);
-
-  ball = Bodies.circle(100,10,20,ball_options);
-  World.add(world,ball);
-
-  ground = Bodies.rectangle(100,400,650,20,ground_options);
-  World.add(world,ground); 
-  
-  
-  
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -55,8 +31,7 @@ function draw()
 
 
   
-  ellipse(ball.position.x,ball.position.y,20);
-  rect(ground.position.x,ground.position.y,650,20)  
+
 }
 
 function vForce()
